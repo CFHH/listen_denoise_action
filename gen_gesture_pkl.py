621cc0a71687ac9af7ca5205189d90e9b3af70a5
CFHH <13436361@qq.com>
2023-09-21 17:19:41 +0800

    改个名

--- gen_gesture_pkl.py
+++ gen_gesture_pkl.py
@@ -36,7 +36,7 @@ def dance_feats_to_bvh(pred_clips):
     return bvh_data
 
 
-def gesture_feats_to_bvh(pred_clips, parameterizer, motions_cols, root_transformer, mocap_data_sample):
+def gesture_feats_to_bvh(pred_clips, parameterizer, motions_cols, mocap_data_sample):
     ignored_joints = ['RightToeBase', 'RightForeFoot', 'LeftToeBase', 'LeftForeFoot', 'pCube4']
     hands = ['LeftHand', 'RightHand']
     fingers = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -53,11 +53,6 @@ def gesture_feats_to_bvh(pred_clips, parameterizer, motions_cols, root_transform
         ignored_columns.append(joint + '_alpha')
         ignored_columns.append(joint + '_beta')
         ignored_columns.append(joint + '_gamma')
-    # 以下3个是root_transformer需要的
-    ignored_columns.append('Hips_Xposition')
-    ignored_columns.append('Hips_Zposition')
-    ignored_columns.append('reference_dYposition')
-    ignored_columns.append('Hips_Yrotation')
 
     full_columns = motions_cols + ignored_columns
 
@@ -81,15 +76,7 @@ def gesture_feats_to_bvh(pred_clips, parameterizer, motions_cols, root_transform
     #new_data.take_name = ''
 
     new_datas = [new_data]
-    temp_datas = parameterizer.inverse_transform(new_datas)
-    my_bvh_datas = root_transformer.inverse_transform(temp_datas)
-    # 这些没有啊
-    if root_transformer.separate_root:
-        my_bvh_datas[0].values['Hips_Xposition'] = my_bvh_datas[0].values['reference_Xposition']
-        my_bvh_datas[0].values['Hips_Zposition'] = my_bvh_datas[0].values['reference_Zosition']
-        my_bvh_datas[0].values['Hips_Yrotation'] = my_bvh_datas[0].values['reference_Yrotation']
-    else:
-        pass
+    my_bvh_datas = parameterizer.inverse_transform(new_datas)
     return my_bvh_datas
 
 
@@ -110,7 +97,7 @@ def process_motion():
     # 这算成30fps
     bvh_data.values = bvh_data.values[::2]
     bvh_data.framerate = bvh_data.framerate * 2
-    # xz
+    # 初始站位归零
     bvh_data.values['Hips_Xposition'] = bvh_data.values['Hips_Xposition'] - bvh_data.values['Hips_Xposition'][0]
     bvh_data.values['Hips_Zposition'] = bvh_data.values['Hips_Zposition'] - bvh_data.values['Hips_Zposition'][0]
     # 保存一下
@@ -126,31 +113,6 @@ def process_motion():
     test_bvh_datas_1 = parameterizer.inverse_transform(expmap_datas)
     bvh_diff_1 = test_bvh_datas_1[0].values - bvh_data.values
 
-    # 取reference参数
-    root_transformer = RootTransformer('pos_rot_deltas', separate_root=False) # pos_xyz_rot_deltas pos_rot_deltas
-    trans_datas = root_transformer.fit_transform(bvh_datas)
-    trans_data = trans_datas[0]
-    my_pkl_data['reference_dXposition'] = trans_data.values['reference_dXposition']
-    my_pkl_data['reference_dZposition'] = trans_data.values['reference_dZposition']
-    my_pkl_data['reference_dYrotation'] = trans_data.values['reference_dYrotation']
-    #测试
-    test_bvh_datas = root_transformer.inverse_transform(trans_datas)
-    if root_transformer.separate_root:
-        test_Hips_Xposition = bvh_data.values['Hips_Xposition'] - bvh_data.values['Hips_Xposition'][0]
-        test_Hips_Zposition = bvh_data.values['Hips_Zposition'] - bvh_data.values['Hips_Zposition'][0]
-        test_Hips_Yrotation = bvh_data.values['Hips_Yrotation'] - bvh_data.values['Hips_Yrotation'][0]
-        test1 = test_bvh_datas[0].values['reference_Xposition'] - test_Hips_Xposition
-        test2 = test_bvh_datas[0].values['reference_Zposition'] - test_Hips_Zposition
-        test3 = test_bvh_datas[0].values['reference_Yrotation'] - test_Hips_Yrotation
-        max1 = np.max(np.abs(test1.values))
-        max2 = np.max(np.abs(test2.values))
-        max3 = np.max(np.abs(test3.values))
-    else:
-        test_bvh_data = test_bvh_datas[0]
-        test_bvh_data.values['Hips_Xposition'] -= test_bvh_data.values['Hips_Xposition'][0]
-        test_bvh_data.values['Hips_Zposition'] -= test_bvh_data.values['Hips_Zposition'][0]
-        bvh_diff = test_bvh_data.values - bvh_data.values
-
     # 实际使用的骨骼
     motions_cols = np.loadtxt(bone_feature_filename, dtype=str).tolist()
     my_pkl_data_final = my_pkl_data[motions_cols]
@@ -161,7 +123,7 @@ def process_motion():
     if is_dance_data:
         my_bvh_datas = dance_feats_to_bvh(my_clips)
     else:
-        my_bvh_datas = gesture_feats_to_bvh(my_clips, parameterizer, motions_cols, root_transformer, expmap_data)
+        my_bvh_datas = gesture_feats_to_bvh(my_clips, parameterizer, motions_cols, expmap_data)
     my_bvh_data = my_bvh_datas[0]
     #my_bvh_diff = my_bvh_data.values[bvh_data.values.columns].values - bvh_data.values[bvh_data.values.columns].values
     my_bvh_data.values['Hips_Xposition'] += bvh_data.values['Hips_Xposition'][0] - my_bvh_data.values['Hips_Xposition'][0]
