fb25e97116ec36e14ab6ff51c9e8835c09a799ff
CFHH <13436361@qq.com>
2023-09-22 14:10:39 +0800

    备份

--- gen_gesture_pkl.py
+++ gen_gesture_pkl.py
@@ -1,42 +1,23 @@
 import os
-import random
-import argparse
-import json
-import torch
-import torch.utils.data
-import sys
-from pathlib import Path
+import shutil
+import tqdm
+import glob
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import pickle as pkl
-import pandas as pd
 from scipy import interpolate
-import joblib as jl
 import librosa
 import copy
+import joblib as jl
+import pandas as pd
 from pymo.data import MocapData
 from pymo.parsers import BVHParser
 from pymo.writers import BVHWriter
 from pymo.preprocessing import MocapParameterizer, RootTransformer
 
 
-def dataframe_nansinf2zeros(df):
-    df.fillna(0, inplace=True)  # 用0填充NA/NaN
-    df.replace([np.inf, -np.inf], 0, inplace=True) # 用0填充np.inf和-np.inf
-    return df
 
-
-def dance_feats_to_bvh(pred_clips):
-    # import pdb;pdb.set_trace()
-    data_pipeline = jl.load('./data/motorica_dance/data_pipe.expmap_30fps.sav')
-    n_feats = data_pipeline["cnt"].n_features
-    data_pipeline["root"].separate_root = False
-    print('inverse_transform...')
-    bvh_data = data_pipeline.inverse_transform(pred_clips[:, :, :n_feats])
-    return bvh_data
-
-
-def gesture_feats_to_bvh(pred_clips, parameterizer, motions_cols, mocap_data_sample):
+def gesture_feats_to_bvh(pred_clips, parameterizer, motions_columns, mocap_data_sample):
     ignored_joints = ['RightToeBase', 'RightForeFoot', 'LeftToeBase', 'LeftForeFoot', 'pCube4']
     hands = ['LeftHand', 'RightHand']
     fingers = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -54,7 +35,7 @@ def gesture_feats_to_bvh(pred_clips, parameterizer, motions_cols, mocap_data_sam
         ignored_columns.append(joint + '_beta')
         ignored_columns.append(joint + '_gamma')
 
-    full_columns = motions_cols + ignored_columns
+    full_columns = motions_columns + ignored_columns
 
     clip = pred_clips[0]
     frames = clip.shape[0]
@@ -86,10 +67,16 @@ def write_bvh(bvh_data, fname):
         writer.write(bvh_data, f)
 
 
-def process_motion():
-    # 一些文件
-    bvh_filename = './data/speech_gesture/TestSeq010.bvh'
-    bone_feature_filename = './data/speech_gesture/pose_features.expmap.txt'
+def process_motion(bvh_filename, motions_cols, save_path, all_files):
+    motion_name = os.path.basename(bvh_filename)
+    motion_name = motion_name.split('.')[0]
+
+    # 跳过已经有的
+    temp_name = motion_name + '_00'
+    all_files.append(temp_name)
+    save_name_1 = os.path.join(save_path, motion_name + '_00.expmap_30fps.pkl')
+    if os.path.isfile(save_name_1):
+        return
 
     # 加载bvh文件
     bvh_parser = BVHParser()
@@ -98,40 +85,60 @@ def process_motion():
     bvh_data.values = bvh_data.values[::2]
     bvh_data.framerate = bvh_data.framerate * 2
     # 初始站位归零
-    bvh_data.values['Hips_Xposition'] = bvh_data.values['Hips_Xposition'] - bvh_data.values['Hips_Xposition'][0]
-    bvh_data.values['Hips_Zposition'] = bvh_data.values['Hips_Zposition'] - bvh_data.values['Hips_Zposition'][0]
+    bvh_data.values['Hips_Xposition'] -= bvh_data.values['Hips_Xposition'][0]
+    bvh_data.values['Hips_Zposition'] -= bvh_data.values['Hips_Zposition'][0]
     # 保存一下
-    write_bvh(bvh_data, './raw.bvh')
     bvh_datas = [bvh_data]
 
-    # 取alpha、beta、gamma参数
+    # 算alpha、beta、gamma参数
     parameterizer = MocapParameterizer('expmap')
     expmap_datas = parameterizer.fit_transform(bvh_datas)
     expmap_data = expmap_datas[0]
-    my_pkl_data = expmap_data.values  # 没有['reference_dXposition']
-    # 测试
-    test_bvh_datas_1 = parameterizer.inverse_transform(expmap_datas)
-    bvh_diff_1 = test_bvh_datas_1[0].values - bvh_data.values
+    full_pkl_data = expmap_data.values
 
-    # 实际使用的骨骼
-    motions_cols = np.loadtxt(bone_feature_filename, dtype=str).tolist()
-    my_pkl_data_final = my_pkl_data[motions_cols]
+    # TODO dxposition、dzposition、dyrotation
 
-    #pkl_data转回bvh
-    my_clips = my_pkl_data_final.values[np.newaxis, ...]
-    is_dance_data = False
-    if is_dance_data:
-        my_bvh_datas = dance_feats_to_bvh(my_clips)
-    else:
+    # 实际训练的的骨骼
+    panda_data = full_pkl_data[motions_cols]
+
+    # 写pkl
+    with open(save_name_1, 'wb') as pkl_f1:
+        pkl.dump(panda_data, pkl_f1)
+
+    dotest = True
+    if dotest:
+        # 加载pkl
+        with open(save_name_1, 'rb') as ff:
+            reload_panda_data = pkl.load(ff).astype('float32')
+        diff = reload_panda_data - panda_data
+
+        # pkl_data转回bvh
+        my_clips = reload_panda_data.values[np.newaxis, ...]
         my_bvh_datas = gesture_feats_to_bvh(my_clips, parameterizer, motions_cols, expmap_data)
-    my_bvh_data = my_bvh_datas[0]
-    #my_bvh_diff = my_bvh_data.values[bvh_data.values.columns].values - bvh_data.values[bvh_data.values.columns].values
-    my_bvh_data.values['Hips_Xposition'] += bvh_data.values['Hips_Xposition'][0] - my_bvh_data.values['Hips_Xposition'][0]
-    my_bvh_data.values['Hips_Zposition'] += bvh_data.values['Hips_Zposition'][0] - my_bvh_data.values['Hips_Zposition'][0]
-    write_bvh(my_bvh_data, './mine.bvh')
-
+        my_bvh_data = my_bvh_datas[0]
+        my_bvh_diff = my_bvh_data.values[bvh_data.values.columns].values - bvh_data.values[bvh_data.values.columns].values
+        my_bvh_data.values['Hips_Xposition'] += bvh_data.values['Hips_Xposition'][0] - my_bvh_data.values['Hips_Xposition'][0]
+        my_bvh_data.values['Hips_Zposition'] += bvh_data.values['Hips_Zposition'][0] - my_bvh_data.values['Hips_Zposition'][0]
+        write_bvh(my_bvh_data, f'./{motion_name}.bvh')
     return
 
 
+
+def process_new_dataset():
+    save_path = './data/my_gesture_data/'
+
+    bone_feature_filename = './data/my_gesture_data/pose_features.expmap.txt'
+    motions_cols = np.loadtxt(bone_feature_filename, dtype=str).tolist()
+
+    # 处理test
+    all_files = []
+    motion_files = glob.glob('./data/my_gesture_data/GENEA/test/motion/*.bvh')
+    motion_files.sort()
+    for bvh_filename in tqdm.tqdm(motion_files):
+        print("Process %s" % bvh_filename)
+        process_motion(bvh_filename, motions_cols, save_path, all_files)
+    # 处理train
+
+
 if __name__ == "__main__":
-    process_motion()
+    process_new_dataset()
